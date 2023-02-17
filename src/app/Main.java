20accac16c087e17f2b18de2bcd5a9be8fd0e5fa
Yure Nobrega <yurenobrega221b@gmail.com>
2023-02-17 15:37:58 -0300

    Primeira classe: Posicao

---
+++ src/app/Main.java
@@ -0,0 +1,13 @@
+package app;
+
+import tabuleiro.Posicao;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Posicao pos = new Posicao(3, 5);
+		System.out.println(pos);
+	}
+
+}
